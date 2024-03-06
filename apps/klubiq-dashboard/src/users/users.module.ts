0a73878618bf7ff4cca214913e8a43e87462b4fc
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-03-06 18:45:39 -0500

    added entities for autoloading

--- apps/klubiq-dashboard/src/users/users.module.ts
+++ apps/klubiq-dashboard/src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from '@app/common';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -2,24 +2,30 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '../config/config.module';
+import { Role } from './entities/role.entity';
+import { UserProfile } from './entities/user-profile.entity';
+import { Permission } from './entities/permission.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        autoLoadEntities: true,
-        synchronize: configService.get<string>('ENV') === 'local',
-        migrations: ['**/*'],
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+	imports: [
+		TypeOrmModule.forFeature([Role, UserProfile, Permission]),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get<string>('DATABASE_HOST'),
+				port: configService.get<number>('DATABASE_PORT'),
+				username: configService.get<string>('DATABASE_USERNAME'),
+				password: configService.get<string>('DATABASE_PASSWORD'),
+				database: configService.get<string>('DATABASE_NAME'),
+				autoLoadEntities: true,
+				synchronize: true,
+				migrations: ['**/migrations'],
+				// configService.get<string>('ENV') === 'local',
+				//
+			}),
+			inject: [ConfigService],
+		}),
+	],
 })
 export class DatabaseModule {}
