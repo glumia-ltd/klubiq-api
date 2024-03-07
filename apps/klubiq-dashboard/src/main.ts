97d44b8b83931152586fc9d83758224ad9696197
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-03-07 11:44:40 -0500

    swagger config update

--- apps/klubiq-dashboard/src/main.ts
+++ apps/klubiq-dashboard/src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { KlubiqDashboardModule } from './klubiq-dashboard.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(KlubiqDashboardModule);
 
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
   const config = new DocumentBuilder()
     .setTitle('Klubiq PMS')
     .setDescription('Klubiq PMS API')
     .setVersion('1.0')
     .addTag('klubiq')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
