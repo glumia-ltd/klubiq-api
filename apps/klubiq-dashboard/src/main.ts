0c4018c8046f5f04270d346acdfe52429f2fe645
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-03-07 11:39:58 -0500

    added swagger config

--- apps/klubiq-dashboard/src/main.ts
+++ apps/klubiq-dashboard/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { KlubiqDashboardModule } from './klubiq-dashboard.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(KlubiqDashboardModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Klubiq PMS')
+    .setDescription('Klubiq PMS API')
+    .setVersion('1.0')
+    .addTag('klubiq')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
