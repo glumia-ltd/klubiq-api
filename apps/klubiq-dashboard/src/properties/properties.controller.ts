842ea2ae96d6203fbf7bcdbe1d5917004088554c
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-04-15 18:28:20 -0400

    Update ECS_SERVICE name to API-Service in klubiq-dashboard-aws.yaml

--- apps/klubiq-dashboard/src/properties/properties.controller.ts
+++ apps/klubiq-dashboard/src/properties/properties.controller.ts
@@ -5,3 +5,12 @@ import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 @ApiBearerAuth()
 @Controller('properties')
 export class PropertiesController {}
+// create property
+// get all properties by organization with pagination
+// get all properties by propertyManager with pagination
+// get all properties by filter with pagination
+// get property by id
+// update property
+// delete property
+// archive property
+// create property for organization
