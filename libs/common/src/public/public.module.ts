a46f00465073a3b4db6b349aaf9e571856f0e274
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-04-22 11:48:14 -0400

    Add PermissionsService, PermissionsRepository, OrganizationRolesRepository, and FeaturesRepository to PublicModule

--- libs/common/src/public/public.module.ts
+++ libs/common/src/public/public.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PublicController } from './public.controller';
+import { PermissionsService } from '../permissions/permissions.service';
+import { PermissionsRepository } from '../repositories/permissions.repository';
+import { OrganizationRolesRepository } from '../repositories/organization-roles.repository';
+import { FeaturesRepository } from '../repositories/features.repository';
 
 @Module({
 	controllers: [PublicController],
+	providers: [
+		PermissionsService,
+		PermissionsRepository,
+		OrganizationRolesRepository,
+		FeaturesRepository,
+	],
 })
 export class PublicModule {}
