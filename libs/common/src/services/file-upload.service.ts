bdb5b8aca175c43ff5a319981fb0bc50318d3284
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-08-08 19:25:27 -0400

    chore: Update lease controller constructor and add ApiBody decorator

--- libs/common/src/services/file-upload.service.ts
+++ libs/common/src/services/file-upload.service.ts
@@ -1,29 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as AWS from 'aws-sdk';
 import { FileUploadDto } from '../dto/requests/file-upload.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class FileUploadService {
-	private readonly s3: AWS.S3;
+	private readonly s3client: S3Client;
 	constructor(private readonly configService: ConfigService) {
-		AWS.config.update({
-			accessKeyId: this.configService.get<string>('AWS_S3_ACCESS_KEY_ID'),
-			secretAccessKey: this.configService.get<string>(
-				'AWS_S3_SECRET_ACCESS_KEY',
-			),
+		this.s3client = new S3Client({
+			credentials: {
+				accessKeyId: this.configService.get<string>('AWS_S3_ACCESS_KEY_ID'),
+				secretAccessKey: this.configService.get<string>(
+					'AWS_S3_SECRET_ACCESS_KEY',
+				),
+			},
 			region: this.configService.get<string>('AWS_S3_REGION'),
 		});
-		this.s3 = new AWS.S3();
 	}
 	async generatePresignedUrl(fileDataDto: FileUploadDto, bucketName: string) {
-		const key = `${fileDataDto.organization}/${fileDataDto.fileName}`;
-		const params = {
+		const key = `${fileDataDto.organization}/${uuidv4()}.${fileDataDto.fileType}`;
+		console.log('KEY: ', key);
+		const command = new PutObjectCommand({
 			Bucket: bucketName,
 			Key: key,
-			Expires: 60 * 5, // 5 minutes
-			contentType: fileDataDto.fileType,
-		};
-		return this.s3.getSignedUrlPromise('putObject', params);
+		});
+
+		console.log('UPLOAD PARAMS: ', command);
+		const presignedUrl = getSignedUrl(this.s3client, command, {
+			expiresIn: 600,
+		});
+		return presignedUrl;
 	}
+
+	// async createFolderIfNotExist(folderName: string, bucketName: string) {
+	//   const params = {
+	//     Bucket: bucketName,
+	//     Key: folderName
+	//   };
+	//   return this.s3.headObject(params).promise();
+	// }
 }
