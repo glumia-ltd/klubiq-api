b2ddd9b528418c1c9e2a6fdff446a739e25b44f0
Vincent Adeyemi <vinadeyems@gmail.com>
2025-04-29 16:07:01 -0400

    Add Generators to AuthModule for enhanced lease name generation

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -27,6 +27,7 @@ import { AuthMiddleware } from './auth.middleware';
 import { LeaseService } from 'apps/klubiq-dashboard/src/lease/services/lease.service';
 import { LeaseRepository } from 'apps/klubiq-dashboard/src/lease/repositories/lease.repository';
 import { LeaseTenantRepository } from '@app/common/repositories/leases-tenant.repositiory';
+import { Generators } from '@app/common/helpers/generators';
 interface FirebaseConfig {
 	type: string;
 	project_id: string;
@@ -101,6 +102,7 @@ const firebaseAdminProvider = {
 		AccessControlService,
 		LeaseService,
 		FileUploadService,
+		Generators,
 	],
 	exports: [LandlordAuthService, AccessControlService],
 	controllers: [AuthController],
